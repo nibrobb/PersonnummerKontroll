339e2cfea940425f0389a92c18df2747c52eb6b7
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2023-04-28 17:59:15 +0200

    changed encoding

--- pnrkontrollForWindows.cpp
+++ pnrkontrollForWindows.cpp
@@ -11,18 +11,18 @@ int check_personnummer(char* persnr)
 {
     int personnummer[11] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    const int tallrekke_1[10] = { 3, 7, 6, 1, 8, 9, 4, 5, 2, 1 };     /* Første spesielle tallrekke */
+    const int tallrekke_1[10] = { 3, 7, 6, 1, 8, 9, 4, 5, 2, 1 };     /* FÃ¸rste spesielle tallrekke */
     const int tallrekke_2[11] = { 5, 4, 3, 2, 7, 6, 5, 4, 3, 2, 1 };  /* Andre spesielle tallrekke */
 
 
 
-    std::string oppgifnr("Oppgitt fødselsnummer: ");
+    std::string oppgifnr("Oppgitt fÃ¸dselsnummer: ");
     ::CharToOemA(oppgifnr.c_str(), const_cast<char*>(oppgifnr.c_str()));
     std::cout << oppgifnr;
 
     for (size_t i = 0; i < strlen(persnr); i++)
     {
-        personnummer[i] = persnr[i] - '0';      /* ASCII-tricery for å parse en enkelt int*/
+        personnummer[i] = persnr[i] - '0';      /* ASCII-tricery for Ã¥ parse en enkelt int*/
         printf("%i", personnummer[i]);
     }
 
@@ -52,17 +52,17 @@ int check_personnummer(char* persnr)
 
     int rest_1 = sum_av_produkt_1 % 11;
 
-    std::string first_control("FØRSTE KONTROLLSIFFER (");
+    std::string first_control("FÃ˜RSTE KONTROLLSIFFER (");
     ::CharToOemA(first_control.c_str(), const_cast<char*>(first_control.c_str()));
 
-    /* Forteller om første kontrollsiffer er gyldig*/
+    /* Forteller om fÃ¸rste kontrollsiffer er gyldig*/
     if (rest_1 == 0) {
         std::cout << first_control << personnummer[9] << ") ER GYLDIG\n\n\n";
-        //printf("FØRSTE KONTROLLSIFFER (%i) ER GYLDIG\n\n\n", personnummer[9]);
+        //printf("FÃ˜RSTE KONTROLLSIFFER (%i) ER GYLDIG\n\n\n", personnummer[9]);
     }
     else {
         std::cout << first_control << personnummer[9] << ") ER IKKE GYLDIG\n\n\n";
-        //printf("FØRSTE KONTROLLSIFFER (%i) ER IKKE GYLDIG!\n\n\n", personnummer[9]);
+        //printf("FÃ˜RSTE KONTROLLSIFFER (%i) ER IKKE GYLDIG!\n\n\n", personnummer[9]);
     }
 
 
@@ -99,7 +99,7 @@ int check_personnummer(char* persnr)
     int status = 0; /* This is the status indicator, it will be 1 if the personnummer is valid, 0 otherwise*/
 
 
-    std::string gyldig("GYLDIG FØDSELSNUMMER");
+    std::string gyldig("GYLDIG FÃ˜DSELSNUMMER");
     ::CharToOemA(gyldig.c_str(), const_cast<char*>(gyldig.c_str()));
 
 
@@ -109,20 +109,20 @@ int check_personnummer(char* persnr)
     }
     else {
         std::cout << "\n\nU" << gyldig << std::endl;
-        //printf("\n\nUGLYDIG FØDSELSNUMMER\n");
+        //printf("\n\nUGLYDIG FÃ˜DSELSNUMMER\n");
     }
 
     return status;
 }
 
 
-/* Program for å sjekke kontrollsiffer i personnummer */
+/* Program for Ã¥ sjekke kontrollsiffer i personnummer */
 int main(int argc, char** argv)
 {
     char buffer[11];
 
-    //printf("Vennligst oppgi ditt fødselsnummer (11 siffer): ");
-    std::string oppgi("Vennligst oppgi ditt fødselsnummer: ");
+    //printf("Vennligst oppgi ditt fÃ¸dselsnummer (11 siffer): ");
+    std::string oppgi("Vennligst oppgi ditt fÃ¸dselsnummer: ");
     ::CharToOemA(oppgi.c_str(), const_cast<char*>(oppgi.c_str()) );
 
     std::cout << oppgi;
